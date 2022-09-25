6372d4d91e6383f9c1b09f37c278df42be21f35e
Jørgen Galdal <jorggal@stud.ntnu.no>
2022-09-25 16:23:47 +0200

    Added background.js

---
+++ background.js
@@ -0,0 +1,13 @@
+console.log("Test");
+
+chrome.runtime.reload(
+    function() {
+        alert("Test");
+    }) 
+
+
+document.getElementsByClassName("sc-hKFyIo xNbqL _2EXLlWeqV4rb1eWGmCkD50")[0].addEventListener("click",
+    function(){
+            console.log("Test");
+    }
+)
