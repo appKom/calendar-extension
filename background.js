de2f571761089d1322cfdfe9c84e00e2a3a3e31a
Jørgen Galdal <jorggal@stud.ntnu.no>
2022-09-25 19:58:39 +0200

    Utforsket content_scripts

--- background.js
+++ background.js
@@ -1,13 +1,16 @@
-console.log("Test");
+// chrome.action.onClicked.addListener(function(tab) {
+//     console.log("Test");
+//     chrome.scripting.executeScript({
+//         code: 'document.body.style.backgroundColor = "blue"'
+//     });
+// });
 
-chrome.runtime.reload(
-    function() {
-        alert("Test");
-    }) 
+// chrome.action.onClicked.addListener(function(tab) {
+//     console.log("Test");
+//   });
 
-
-document.getElementsByClassName("sc-hKFyIo xNbqL _2EXLlWeqV4rb1eWGmCkD50")[0].addEventListener("click",
-    function(){
-            console.log("Test");
-    }
-)
+// document.getElementsByClassName("sc-hKFyIo xNbqL _2EXLlWeqV4rb1eWGmCkD50")[0].addEventListener("click",
+//     function(){
+//             console.log("Test");
+//     }
+// )
---
+++ test.js
@@ -0,0 +1,7 @@
+// document.body.style.backgroundColor = "blue";
+
+document.querySelector("._2EXLlWeqV4rb1eWGmCkD50").addEventListener("click",
+    function(){
+            console.log("Test");
+    }
+)
