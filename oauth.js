c2ae07139e3859cf1acb7601d908826c2542eef2
Jørgen Galdal <jorggal@stud.ntnu.no>
2022-10-09 14:49:24 +0200

    Test for scope calendar

--- oauth.js
+++ oauth.js
@@ -1,7 +1,22 @@
 window.onload = function() {
-    document.querySelector('button').addEventListener('click', function() {
+    document.getElementById("gammel-knapp").addEventListener('click', function() {
       chrome.identity.getAuthToken({interactive: true}, function(token) {
         console.log(token);
+        var init = { 
+            'method' : 'GET',
+            'async'  : true,
+            'headers': {
+              'Authorization' : 'Bearer ' + token,
+              'Content-Type': 'application/json'
+            },
+            'contentType': 'json'
+          };
+        
+          fetch('https://www.googleapis.com/calendar/v3/calendars/public/events', init)
+          .then((response) => response.json()) // Transform the data into json
+          .then(function(data) {
+              console.log(data);
+            })
       });
     });
   };
