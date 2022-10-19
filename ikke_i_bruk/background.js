1af9c0337f759ce3c641cd1484201d785db9c65e
Jørgen Galdal <jorggal@stud.ntnu.no>
2022-10-19 11:34:46 +0200

    Insert request, men fungerer ikke

--- ikke_i_bruk/background.js
+++ ikke_i_bruk/background.js
@@ -1,3 +1,11 @@
-chrome.action.onClicked.addListener(function() {
-    chrome.tabs.create({url: 'index.html'});
-  });
+const API_KEY = "AIzaSyCL3vj18BOFVjgfPjHUEMxYfcxqwKZOpss";
+const DISCOVERY_DOCS = 'https://www.googleapis.com/discovery/v1/apis/calendar/v3/rest';
+
+
+
+
+// chrome.action.onClicked.addListener(function() {
+//     chrome.tabs.create({url: 'index.html'});
+//   });
+
+
--- oauth.js
+++ oauth.js
@@ -1,18 +1,35 @@
+function onGAPILoad() {
+  gapi.client.init({
+    // Don't pass client nor scope as these will init auth2, which we don't want
+    apiKey: API_KEY,
+    discoveryDocs: DISCOVERY_DOCS,
+  }).then(function () {
+    console.log('gapi initialized')
+  }, function(error) {
+    console.log('error', error)
+  });
+}
+
+
 window.onload = function() {
     document.getElementById("gammel-knapp").addEventListener('click', function() {
       chrome.identity.getAuthToken({interactive: true}, function(token) {
         console.log(token);
         var init = { 
-            'method' : 'GET',
+            'method' : 'INSERT',
             'async'  : true,
             'headers': {
               'Authorization' : 'Bearer ' + token,
+              'Accept': 'application/json',
               'Content-Type': 'application/json'
             },
-            'contentType': 'json'
+            'contentType': 'json',
+            "body": {
+              "summary": "test"
+            }
           };
         
-          fetch('https://www.googleapis.com/calendar/v3/calendars/public/events', init)
+          fetch('https://www.googleapis.com/calendar/v3/calendars?key=AIzaSyCL3vj18BOFVjgfPjHUEMxYfcxqwKZOpss', init)
           .then((response) => response.json()) // Transform the data into json
           .then(function(data) {
               console.log(data);
