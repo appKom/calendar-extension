013a6b55e515b26e93173c27628e05c45f8eb9fb
Julian <julianao@stud.ntnu.no>
2023-01-28 21:40:02 +0100

    Formaterte filene + noen kommentarer

--- addEvent.js
+++ addEvent.js
@@ -4,38 +4,41 @@
 // Her legger vi til en eventlistener på siden vi befinner oss på.
 
 async function postEvent() {
-    /* Sender request til extensionen (popup-en) og 
-    ber om API-key og user-token tilbake. 
+  /* Sender request til extensionen (popup-en) og
+    ber om API-key og user-token tilbake.
     Caller så postEvent med riktige credentials */
 
-    const extension_id = "elijkjhoojegfcnehlpgbkacplephicj"
-    
-    chrome.runtime.sendMessage(extension_id, {request: "credentials"}, async function (response) {
-        console.log("API_KEY i getCredentials: " + response.API_KEY)
-        console.log("token i getCredentials: " + response.token)
-        await postEventInner(response)
-    })
+  const extension_id = "elijkjhoojegfcnehlpgbkacplephicj"
+
+  chrome.runtime.sendMessage(
+    extension_id,
+    { request: "credentials" },
+    async function (response) {
+      console.log("API_KEY i getCredentials: " + response.API_KEY)
+      console.log("token i getCredentials: " + response.token)
+      await postEventInner(response)
+    }
+  )
 }
 
-
 async function postEventInner(credentials) {
-    // Poster event til brukers online-kalender
-    // console.log("credentials i postEvent: " + credentials)
-    const API_KEY = credentials.API_KEY
-    const token = credentials.token
-    
-    chrome.storage.local.get(["online_calendar_id"], (result) => {
-        const online_calendar_id = result.online_calendar_id    
-        
-        var init = {
-            'method': 'POST',
-            'async': true,
-            'headers': {
-                'Authorization': 'Bearer ' + token,
-                'Accept': 'application/json',
-                'Content-Type': 'application/json',
-            },
-            'body': `{
+  // Poster event til brukers online-kalender
+  // console.log("credentials i postEvent: " + credentials)
+  const API_KEY = credentials.API_KEY
+  const token = credentials.token
+
+  chrome.storage.local.get(["online_calendar_id"], (result) => {
+    const online_calendar_id = result.online_calendar_id
+
+    var init = {
+      method: "POST",
+      async: true,
+      headers: {
+        Authorization: "Bearer " + token,
+        Accept: "application/json",
+        "Content-Type": "application/json",
+      },
+      body: `{
                 "start": {
                     "dateTime": "2022-11-28T09:00:00-08:00",
                     "timeZone": "America/Los_Angeles"
@@ -44,30 +47,34 @@ async function postEventInner(credentials) {
                     "dateTime": "2022-11-28T09:00:00-09:00",
                     "timeZone": "America/Los_Angeles"
                 }
-            }`
-        }
-        
-        console.log("Test inni postEvent")
-        
-        fetch("https://www.googleapis.com/calendar/v3/calendars/" + online_calendar_id + "/events?key=" + API_KEY, init)
-        .then((response) => response.json())
-        .then((data) => {
-            console.log("Test inni fetch")
-            console.log(data)
-        })
-    })
-}
-    
-document.addEventListener("click", (e) => {
+            }`,
+    }
 
-    // her trekker vi ut teksten til knappen som ble klikket på, og sjekker om det samsvarer med den knappen vi ønsker å gi funksjonalitet til.
-    postEvent();
-    if (e.target.innerText == "Meld meg på") {
-        console.log("Du har nå blitt meldt på! ");
-    }
-    else if (e.target.innerText == "Meld meg på venteliste") {
-        console.log("Du har nå blitt satt på venteliste. Du er nummer # i køen");
-    }
+    console.log("Test inni postEvent")
+
+    fetch(
+      "https://www.googleapis.com/calendar/v3/calendars/" +
+        online_calendar_id +
+        "/events?key=" +
+        API_KEY,
+      init
+    )
+      .then((response) => response.json())
+      .then((data) => {
+        console.log("Test inni fetch")
+        console.log(data)
+      })
+  })
+}
+
+document.addEventListener("click", (e) => {
+  // her trekker vi ut teksten til knappen som ble klikket på, og sjekker om det samsvarer med den knappen vi ønsker å gi funksjonalitet til.
+  postEvent()
+  if (e.target.innerText == "Meld meg på") {
+    console.log("Du har nå blitt meldt på! ")
+  } else if (e.target.innerText == "Meld meg på venteliste") {
+    console.log("Du har nå blitt satt på venteliste. Du er nummer # i køen")
+  }
 })
 
 postEvent()
--- background.js
+++ background.js
@@ -14,18 +14,21 @@ async function addNewSecondaryCalendar(token) {
   // Returnerer id-en til opprettet kalender.
   console.log("Adding new secondary calendar")
   var init = {
-    'method': 'POST',
-    'async': true,
-    'headers': {
-      'Authorization': 'Bearer ' + token,
-      'Accept': 'application/json',
-      'Content-Type': 'application/json'
+    method: "POST",
+    async: true,
+    headers: {
+      Authorization: "Bearer " + token,
+      Accept: "application/json",
+      "Content-Type": "application/json",
     },
     // 'contentType': 'json',
-    'body': "{'summary': 'Extension-test'}"
-  };
+    body: "{'summary': 'Extension-test'}",
+  }
 
-  response = await fetch('https://www.googleapis.com/calendar/v3/calendars?alt=json&key=' + API_KEY, init)
+  response = await fetch(
+    "https://www.googleapis.com/calendar/v3/calendars?alt=json&key=" + API_KEY,
+    init
+  )
   data = await response.json()
   return data
 }
@@ -34,39 +37,43 @@ function check_if_calendar_exists(data, calendar_id) {
   for (var i = 0; i < data.items.length; i++) {
     console.log(data.items[i].id)
     if (data.items[i].id == calendar_id) {
-      return true;
+      return true
     }
   }
-  return false;
-};
+  return false
+}
 
 async function doAuth() {
   chrome.storage.local.get(["online_calendar_id"], (result) => {
     const online_calendar_id = result.online_calendar_id
     console.log("online_calendar_id i doAuth: " + online_calendar_id)
 
-    chrome.identity.getAuthToken({ interactive: true }, function (token) { // GET
+    chrome.identity.getAuthToken({ interactive: true }, function (token) {
+      // GET
       API_KEY = "AIzaSyCL3vj18BOFVjgfPjHUEMxYfcxqwKZOpss"
       chrome.storage.local.set({ API_KEY: API_KEY })
       chrome.storage.local.set({ token: token })
 
-      console.log("token: " + token);
+      console.log("token: " + token)
 
       var init = {
-        'method': 'GET',
-        'async': true,
-        'headers': {
-          'Authorization': 'Bearer ' + token,
-          'Accept': 'application/json',
-          'Content-Type': 'application/json'
+        method: "GET",
+        async: true,
+        headers: {
+          Authorization: "Bearer " + token,
+          Accept: "application/json",
+          "Content-Type": "application/json",
         },
-        'contentType': 'json'
-      };
+        contentType: "json",
+      }
 
-      fetch('https://www.googleapis.com/calendar/v3/users/me/calendarList', init)
+      fetch(
+        "https://www.googleapis.com/calendar/v3/users/me/calendarList",
+        init
+      )
         .then((response) => response.json()) // Transform the data into json
         .then(function (data) {
-          console.log(data);
+          console.log(data)
 
           online_calendar_exists = false
           if (online_calendar_id) {
@@ -76,45 +83,43 @@ async function doAuth() {
             } else {
               console.log("Calendar does not exist")
             }
-          } else { console.log("No calendarID in chrome.storage") }
+          } else {
+            console.log("No calendarID in chrome.storage")
+          }
 
-          if (!online_calendar_exists) { // CREATE NEW CALENDAR
+          if (!online_calendar_exists) {
+            // CREATE NEW CALENDAR
             addCalendarAndStoreCalendarId(token)
-
           }
         })
     })
   })
 }
 
-chrome.runtime.onMessage.addListener(
-  function (request, sender, sendResponse) {
-    // console.log(sender.tab ?
-    //             "from a content script:" + sender.tab.url :
-    //             "from the extension")
+chrome.runtime.onMessage.addListener(function (request, sender, sendResponse) {
+  // console.log(sender.tab ?
+  //             "from a content script:" + sender.tab.url :
+  //             "from the extension")
 
-    if (request.request === "oauth") {
-      doAuth()
-      return true
-    }
-
-    if (request.request === "credentials") {
-      chrome.storage.local.get(["token"], (result) => {
-        const token = result.token
-        chrome.storage.local.get(["API_KEY"], (result) => { 
-          const API_KEY = result.API_KEY
-          // token = localStorage.getItem("user-token")
-          // API_KEY = localStorage.getItem("API_KEY")
-          
-          sendResponse({
-            API_KEY: API_KEY,
-            token: token
-          })
-        })
-        
-        
-      })
-      return true
-    }
+  if (request.request === "oauth") {
+    doAuth()
+    return true
   }
-);
+
+  if (request.request === "credentials") {
+    chrome.storage.local.get(["token"], (result) => {
+      const token = result.token
+      chrome.storage.local.get(["API_KEY"], (result) => {
+        const API_KEY = result.API_KEY
+        // token = localStorage.getItem("user-token")
+        // API_KEY = localStorage.getItem("API_KEY")
+
+        sendResponse({
+          API_KEY: API_KEY,
+          token: token,
+        })
+      })
+    })
+    return true
+  }
+})
