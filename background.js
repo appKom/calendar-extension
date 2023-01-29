8ea1652fda651990277df827c97d0fb021751c95
Jørgen Galdal <jorggal@stud.ntnu.no>
2023-01-29 15:40:32 +0100

    Kommentarer til background.js

--- background.js
+++ background.js
@@ -11,7 +11,7 @@ async function addCalendarAndStoreCalendarId(token) {
 }
 
 async function addNewSecondaryCalendar(token) {
-  // Returnerer id-en til opprettet kalender.
+  // Returnerer id-en til opprettet kalender basert på token.
   console.log("Adding new secondary calendar")
   var init = {
     method: "POST",
@@ -21,7 +21,6 @@ async function addNewSecondaryCalendar(token) {
       Accept: "application/json",
       "Content-Type": "application/json",
     },
-    // 'contentType': 'json',
     body: "{'summary': 'Extension-test'}",
   }
 
@@ -34,6 +33,8 @@ async function addNewSecondaryCalendar(token) {
 }
 
 function check_if_calendar_exists(data, calendar_id) {
+  /*  */
+
   for (var i = 0; i < data.items.length; i++) {
     console.log(data.items[i].id)
     if (data.items[i].id == calendar_id) {
@@ -44,18 +45,23 @@ function check_if_calendar_exists(data, calendar_id) {
 }
 
 async function doAuth() {
-  chrome.storage.local.get(["online_calendar_id"], (result) => {
-    const online_calendar_id = result.online_calendar_id
-    console.log("online_calendar_id i doAuth: " + online_calendar_id)
+  /* Autentiserer bruker mot Google Calendar-API og får
+  tilbake credentials (token) som lagres i storage.local for å */
 
+  chrome.storage.local.get(["online_calendar_id"], (result) => {
+    // Henter ID-en til kalender
+    const online_calendar_id = result.online_calendar_id
+
+    /* getAuthToken åpner interaktivt vindu hvor man logger inn med
+    Google-bruker. */
     chrome.identity.getAuthToken({ interactive: true }, function (token) {
-      // GET
-      API_KEY = "AIzaSyCL3vj18BOFVjgfPjHUEMxYfcxqwKZOpss"
+      
+      // Lagrer API_KEY og token for senere bruk.
+      let API_KEY = "AIzaSyCL3vj18BOFVjgfPjHUEMxYfcxqwKZOpss"
       chrome.storage.local.set({ API_KEY: API_KEY })
       chrome.storage.local.set({ token: token })
 
-      console.log("token: " + token)
-
+      // Statiske headers som brukes for GET-request 
       var init = {
         method: "GET",
         async: true,
@@ -67,11 +73,12 @@ async function doAuth() {
         contentType: "json",
       }
 
+      // Fetcher liste over alle kalendere i Google for bruker
       fetch(
         "https://www.googleapis.com/calendar/v3/users/me/calendarList",
         init
       )
-        .then((response) => response.json()) // Transform the data into json
+        .then((response) => response.json()) // Gjør response til json
         .then(function (data) {
           console.log(data)
 
@@ -99,25 +106,25 @@ async function doAuth() {
 
 chrome.runtime.onMessage.addListener(
   /* Handler alle incoming messages fra andre filer som
-   */
+  må handles her grunnet API-restriksjoner */
   function (request, sender, sendResponse) {
-    console.log(sender.tab ?
-                "from a content script:" + sender.tab.url :
-                "from the extension")
 
   if (request.request === "oauth") {
+    /* Kjøres når den får melding fra popup.js om å
+    autentisere. */
     doAuth()
     return true
   }
 
   if (request.request === "credentials") {
+    /* Kjøres når content-scriptet (addEvent.js) brukes 
+    for å poste events. */
     chrome.storage.local.get(["token"], (result) => {
       const token = result.token
       chrome.storage.local.get(["API_KEY"], (result) => {
         const API_KEY = result.API_KEY
-        // token = localStorage.getItem("user-token")
-        // API_KEY = localStorage.getItem("API_KEY")
 
+        // Sender API-key og token 
         sendResponse({
           API_KEY: API_KEY,
           token: token,
